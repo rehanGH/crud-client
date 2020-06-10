ccfc37b91fa9440d7a855c99c80c1bf77afec042
Rehan Khan <rehangh8924@gmail.com>
2020-06-10 13:51:53 -0400

    removed sections from code that used Players

--- src/components/views/AllCampusesView.js
+++ src/components/views/AllCampusesView.js
@@ -1,13 +1,18 @@
 import React from "react";
-// import './styles/AllPlayersView.css';
+import "./styles/AllCampusesView.css";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom"
 
 const AllCampusesView = (props) => {
   return (
     <div className="all-campuses">
+      <h1>{props.hello}</h1>
       {props.allCampuses.map((campus) => (
         <div key={campus.id}>
+          <Link to={`/campuses/${campus.id}`}>
           <h1>{campus.name}</h1>
+          </Link>
+          <p>{campus.description}</p>
         </div>
       ))}
     </div>
